671dedeed7953bf761fb4a407d20ace9f695ff25
Alexandre Halley <alexandre.m.halley@gmail.com>
2022-03-04 18:01:21 +0100

    z effect added

--- js/index.js
+++ js/index.js
@@ -1,80 +1,120 @@
+/* ------- GLOBAL VARIABLES ------- */
+
+const startButton = document.querySelector(".start-btn")
+//const ctx = getAdjustedCanvas()
+const canvasElement = document.querySelector("canvas#canvas")
+const ctx = document.querySelector("canvas#canvas").getContext('2d')
+ctx.canvas.width  = canvasElement.parentNode.offsetWidth
+ctx.canvas.height = canvasElement.parentNode.offsetHeight
+
+let snitchImg = new Image()
+snitchImg.src = '../styles/images/golden_snitch.png'
+let sizeCoefficient = 0.01
+
+
+
 /* ------- CLASSES ------- */
 
-class Chronometer {
+
+class Game {
   constructor() {
-    // ... your code goes here
-    this.currentTime = 0
-    this.intervalId = null
+    this.score = 0
+    this.time = 30
+    this.clockIntervalId = null
+    this.gameIntervalId = null
+    this.isOn = false
   }
 
-  start(callback) {
-    // ... your code goes here
-    if(!callback){
-      this.intervalId = setInterval( () => this.currentTime++,10)
-    } else {
-      this.intervalId = setInterval(callback,10)
+  start() {
+    this.clockIntervalId = setInterval( () => this.time--,1000)
+    this.gameIntervalID = setInterval(refresh, 100)
+    this.isOn=true
     }
-  }
 
-  getMinutes() {
-    // ... your code goes here
-    return Math.floor(this.currentTime/(60*100))
-  }
-
-  getCentiseconds() {
-    // ... your code goes here
-    return this.currentTime % 100
+  stop() {
+    clearInterval(this.clockIntervalId)
+    clearInterval(this.gameIntervalID)
+    this.isOn=false
   }
 
   getSeconds() {
-    // ... your code goes here
-    return Math.floor(this.currentTime/100)
+    return this.time
   }
 
-  computeTwoDigitNumber(value) {
-    // ... your code goes here
-    return String(value).padStart(2, '0')
-  }
-
-  stop() {
-    // ... your code goes here
-    clearInterval(this.intervalId)
-  }
-
-  reset() {
-    // ... your code goes here
-    this.currentTime = 0
-  }
-}
-
-
-class Player {
-    constructor() {
-      this.name = undefined
-      this.score = 0
+  isElapsedTime(){
+    if (this.time <= 0){
+      return true
+    }
+    else {
+      return false
     }
   }
+
+}
 
 class Snitch {
   constructor() {
     this.x = ctx.canvas.width/2
     this.y = ctx.canvas.height/2
-    this.img = new Image()
-    this.img.src = '../styles/images/golden_snitch.png';
+    this.img = snitchImg
     this.velocity = 50
-    this.vAngle = Math.PI/4    
-    this.width = 150
-    this.height = 50
-    this.size = 1
-    this.color = "yellow"
-    
-    this.imgRatio = this.img.width / this.img.height
+    this.vAngle = Math.PI/4  
+    this.width = snitchImg.width *sizeCoefficient
+    this.height = snitchImg.height * sizeCoefficient
+    this.initWidth = snitchImg.width * sizeCoefficient
+    this.initHeight = snitchImg.height * sizeCoefficient
+    this.Z = 0
+    this.sizeEffect = 0
+    this.color = "yellow" 
+  }
+
+
+  stop(){
+    this.velocity = 0
   }
 
   draw(){
-      ctx.drawImage(this.img, this.x, this.y, this.width,this.width/this.imgRatio)
+      ctx.drawImage(this.img, this.x, this.y, this.width,this.height)
+    }
+
+  reset(){
+    this.x = ctx.canvas.width*2/10 +  Math.random()*(ctx.canvas.width*6/10)
+    this.y = ctx.canvas.height*2/10 + Math.random()*(ctx.canvas.height*6/10)
+    this.width = this.initWidth
+    this.height = this.initHeight
+  }
+
+  move() {
+    snitch.vAngle += Math.PI/8 
+  
+    snitch.x = snitch.x + Math.cos(snitch.vAngle)*snitch.velocity;
+    snitch.y = snitch.y + Math.sin(snitch.vAngle)*snitch.velocity;
+  
+  }
+
+  moveZ() {
+      this.Z = 4*Math.sin(this.sizeEffect)
+      this.width = this.initWidth * (1 + this.Z )
+      this.height = this.initHeight * (1 + this.Z )
+      this.sizeEffect += Math.PI*0.1
+    return 
+  }
+
+  isCaught(clickX,clickY){
+    let x0 = this.x + this.width/2
+    let y0 = this.y + this.height/2
+    let radius = this.width/2
+
+    if (((clickX-x0)**2+(clickY-y0)**2) <= radius**2){
+      return true
+    } else {
+      return false
     }
   }
+
+
+
+}
 
 
 /* ------- FUNCTIONS ------- */
@@ -100,27 +140,70 @@ function drawAll() {
   snitch.draw();
 }
 
-function moveSnitch() {
-  snitch.vAngle += Math.PI/200 + Math.random()*Math.PI
 
-  snitch.x = snitch.x + Math.cos(snitch.vAngle)*snitch.velocity;
-  snitch.y = snitch.y + Math.sin(snitch.vAngle)*snitch.velocity;
-
+function updateTimer(){
+  document.querySelector(".chronometer-container").textContent = game.getSeconds()
 }
 
-function launchGame(){
-  clearAll(true)
-  moveSnitch()
+function updateScore(){
+  if (game.isOn){
+    document.querySelector(".start-btn").textContent = game.score
+  }
+}
+
+function endGame(){
+  game.stop()
+  snitch.stop()
+  document.querySelector(".start-btn").textContent = "START"
+}
+
+function refresh(){
+  clearAll()
+  updateScore()
+  updateTimer()
+  snitch.move()
+  snitch.moveZ()
+  if (game.isElapsedTime()){
+    endGame()
+  }
+
   drawAll()
 }
 
+function launchGame(event){
+  if (event.target.textContent === "START"){
+    snitch = new Snitch()
+    game = new Game()
+    game.start()
+  }
+}
+
+
+function tryToCatch(event){
+  const rect = canvas.getBoundingClientRect()
+  const clickX = event.clientX - rect.left
+  const clickY = event.clientY - rect.top
+
+  if (snitch.isCaught(clickX,clickY)){
+    game.score++
+    snitch.reset()
+  }
+  
+}
+
+
+/* ------- LISTENERS ------- */
+
+startButton.addEventListener('click',launchGame)
+
+canvasElement.addEventListener('mousedown', function(event) {
+  tryToCatch(event)
+})
+
 /* ------- INIT ------- */
 
-const ctx = getAdjustedCanvas()
-snitch = new Snitch()
-snitch.draw()
 
-setInterval(launchGame, 100)
+
 
 
 
